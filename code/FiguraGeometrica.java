1bbd61d305095e6acea1d0140603b617e69717a7
alonsosanchezlaura-maker <alonso.sanchez.laura@iescamas.es>
2026-03-09 14:43:13 +0100

    Add files via upload

---
+++ code/FiguraGeometrica.java
@@ -0,0 +1,110 @@
+package com.endes.figuras;
+
+
+
+/**
+ * Clase abstracta que representa una figura geométrica genérica.
+ * Todas las figuras geométricas deben extender esta clase e implementar
+ * los métodos abstractos de cálculo de área y perímetro.
+ * 
+ * Implementa {@link Comparable} para proporcionar un orden natural basado en el área.
+ * 
+ * @author Tu nombre
+ * @version 1.0
+ * @since 1.8 
+ */
+public abstract class FiguraGeometrica implements Comparable<FiguraGeometrica> {
+    /**
+     * Nombre de la figura geométrica.
+     */
+    protected String nombre;
+
+    /**
+     * Color de la figura geométrica.
+     */
+    protected String color;
+
+    /**
+     * Constructor de la clase FiguraGeometrica.
+     * 
+     * @param nombre el nombre de la figura geométrica
+     * @param color el color de la figura geométrica
+     */
+    public FiguraGeometrica(String nombre, String color) {
+        this.nombre = nombre;
+        this.color = color;
+    }
+
+    /**
+     * Obtiene el nombre de la figura geométrica.
+     * 
+     * @return el nombre de la figura
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Obtiene el color de la figura geométrica.
+     * 
+     * @return el color de la figura
+     */
+    public String getColor() {
+        return color;
+    }
+
+    /**
+     * Calcula el área de la figura geométrica.
+     * Este método debe ser implementado por las clases concretas.
+     * 
+     * @return el área de la figura
+     */
+    public abstract double area();
+
+    /**
+     * Calcula el perímetro de la figura geométrica.
+     * Este método debe ser implementado por las clases concretas.
+     * 
+     * @return el perímetro de la figura
+     */
+    public abstract double perimetro();
+
+    /**
+     * Compara esta figura geométrica con otra en función del área.
+     * 
+     * @param o la figura geométrica con la que se comparará
+     * @return un valor negativo si esta figura tiene menor área,
+     *         cero si tienen el mismo área, 
+     *         un valor positivo si esta figura tiene mayor área
+     * @see Comparable
+     */
+    @Override
+    public int compareTo(FiguraGeometrica o) {
+        return Double.compare(this.area(), o.area());
+    }
+
+    /**
+     * Rota la figura geométrica un número específico de grados.
+     * Este método puede ser sobrescrito por clases concretas
+     * para proporcionar una implementación específica.
+     * 
+     * @param angulo el ángulo en grados por el cual rotar la figura
+     * @throws UnsupportedOperationException si la operación no está soportada
+     */
+    public void rotar(double angulo) {
+        System.out.println("Rotando la figura " + nombre + " en " + angulo + " grados.");
+    }
+
+    /**
+     * Traslada la figura geométrica en las direcciones X e Y.
+     * Este método puede ser sobrescrito por clases concretas
+     * para proporcionar una implementación específica.
+     * 
+     * @param dx el desplazamiento en el eje X
+     * @param dy el desplazamiento en el eje Y
+     * @throws UnsupportedOperationException si la operación no está soportada
+     */
+    public void trasladar(double dx, double dy) {
+        System.out.println("Trasladando la figura " + nombre + " en (" + dx + ", " + dy + ").");
+    }
+}
---
+++ code/Punto.java
@@ -0,0 +1,74 @@
+package com.endes.figuras;
+
+public class Punto extends FiguraGeometrica {
+    private Punto centro;
+    private double radio;
+    
+
+    // Constructor
+    public Punto(String nombre, String color, Punto centro, double radio) {
+        super(nombre, color);
+        this.centro = centro;
+        this.radio = radio;
+    }
+
+    // Métodos getters y setters
+    public Punto getCentro() {
+        return centro;
+    }
+
+    public void setCentro(Punto centro) {
+        this.centro = centro;
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+    // Implementación del método area
+    @Override
+    public double area() {
+        return Math.PI * radio * radio;
+    }
+
+    // Implementación del método perimetro
+    @Override
+    public double perimetro() {
+        return 2 * Math.PI * radio;
+    }
+
+    // Implementación del método rotar
+    @Override
+    public void rotar(double angulo) {
+        // No se especifica comportamiento, por lo que no hace nada
+    }
+
+    // Implementación del método trasladar
+    @Override
+    public void trasladar(double dx, double dy) {
+        centro.trasladar(dx, dy);
+    }
+
+    // Método compareTo para comparación de figuras
+    @Override
+    public int compareTo(FiguraGeometrica o) {
+        if (o == null) {
+            return 1;
+        }
+
+        if (o instanceof Punto) {
+            Punto otroCirculo = (Punto) o;
+            if (this.radio < otroCirculo.radio) {
+                return -1;
+            } else if (this.radio > otroCirculo.radio) {
+                return 1;
+            }
+            return 0;
+        }
+        return super.compareTo(o);
+    }
+}
